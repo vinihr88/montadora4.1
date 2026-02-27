572a9328d3253884acdcb7a620f2c767330251f2
vinihr88 <vinicius.h.mendes@aluno.senai.br>
2026-02-27 16:19:10 -0300

    Update login page header and link text

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
     <title>Login - BYD</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body class="login-page"> <header>BYD - Sistema</header>
+<body class="login-page">
 
-<div class="container login-container"> <h2>Login</h2>
-
+<div class="login-panel">
+    <h2>Login do Sistema - BYD</h2>
     <form action="login.php" method="POST">
         <input type="text" name="usuario" placeholder="Usuário" required>
         <input type="password" name="senha" placeholder="Senha" required>
         <button type="submit">Entrar</button>
     </form>
-
-    <p><a href="cadastro.php">Cadastrar</a></p>
+    <p><a href="cadastro.php">Cadastrar usuario</a></p>
 </div>
 
 </body>
